86e6c7c6c86c64a1110f1d475a51be4cb50ff533
Benjamin Snoha <benjamin@snoha.com>
2015-05-29 17:21:32 -0600

    Player now stops when he hits a wall

--- src/cs/lucioben/game/base/Application.java
+++ src/cs/lucioben/game/base/Application.java
@@ -5,8 +5,6 @@ import cs.lucioben.game.scenes.StartScene;
 
 public class Application {
 	public static void main(String[] args) {
-		final int WIDTH = 800;
-		final int HEIGHT = 800;
 		Scene[] scenes = {new StartScene(), new SecondScene()};
 		Game game = Game.getContext();
 		game.start(scenes);

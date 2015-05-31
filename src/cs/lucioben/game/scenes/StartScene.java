ffbe835a4e9472a1732042738147838f52ac4082
Benjamin Snoha <benjamin@snoha.com>
2015-05-30 19:35:30 -0600

    Fixed rotation

--- src/cs/lucioben/game/scenes/StartScene.java
+++ src/cs/lucioben/game/scenes/StartScene.java
@@ -1,7 +1,6 @@
 package cs.lucioben.game.scenes;
 
 import org.lwjgl.util.vector.Vector2f;
-import cs.lucioben.game.GameObjects.SceneChanger;
 import cs.lucioben.game.GameObjects.Player;
 import cs.lucioben.game.GameObjects.base.TextObject;
 import cs.lucioben.game.base.Scene;

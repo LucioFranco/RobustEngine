951e856027ce96afb7823c1beb601bfd71203fb2
Lucio Franco <lucio.franco@du.edu>
2015-06-01 17:41:04 -0600

    added GL commands to fix png transparency

--- src/cs/lucioben/game/base/Game.java
+++ src/cs/lucioben/game/base/Game.java
@@ -79,6 +79,8 @@ public class Game {
 				GL11.glColor3f(255, 255, 255);
 				GL11.glLoadIdentity();
 				GL11.glPushMatrix();
+				GL11.glEnable(GL11.GL_BLEND);
+				GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
 				switch(GameObj.getType()){
 					case PLAYER:

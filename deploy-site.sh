6a98c815005272af08a13802ac1c803cedb90a43
Lucio Franco <lucio.franco@du.edu>
2015-06-16 13:05:32 -0400

    added deploy.sh

---
+++ deploy-site.sh
@@ -0,0 +1,9 @@
+git checkout -b gh-pages
+git checkout gh-pages
+gradle javadoc
+git add --all
+git commit -m "updated gh pages"
+git push origin gh-pages
+git checkout master
+git branch -D gh-pages
+echo "Done!

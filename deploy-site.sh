fb19004f9b75c5c54d531de74e14aa53156a0bcc
Lucio Franco <lucio.franco@du.edu>
2015-06-19 15:51:54 -0400

    fixed deploy script

--- deploy-site.sh
+++ deploy-site.sh
@@ -3,7 +3,7 @@ git checkout gh-pages
 gradle javadoc
 git add --all
 git commit -m "updated gh pages"
-git push origin gh-pages
+git push -f origin gh-pages
 git checkout master
 git branch -D gh-pages
-echo "Done!
+echo "Done!"
